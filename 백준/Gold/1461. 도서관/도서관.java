48eb135f45dbce26a3cebad949ceda17a4816348
guswls <yodg3456@naver.com>
2022-04-21 20:37:06 +0900

    [Gold V] Title: 도서관, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		ArrayList<Integer> plus = new ArrayList<>();
+		ArrayList<Integer> minus = new ArrayList<>();
+		int result = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			if (a < 0)
+				minus.add(a);
+			else
+				plus.add(a);
+		}
+		// 마이너스랑 플러스 나눠서 넣기
+		Collections.sort(plus, Collections.reverseOrder());// 플러스는 내림차순으로 정렬
+		Collections.sort(minus);
+
+		for (int i = 0; i < minus.size(); i += M) {
+			result += (Math.abs(minus.get(i))) * 2;
+		}
+		// 마이너스 계산
+		for (int i = 0; i < plus.size(); i += M) {
+			result += (Math.abs(plus.get(i))) * 2;
+		} // 플러스 계산
+
+		if (!minus.isEmpty() & !plus.isEmpty()) {
+			if (Math.abs(minus.get(0)) >= Math.abs(plus.get(0))) {
+				// 마이너스가 크니까 한번 더했던걸 빼주기
+				result -= (Math.abs(minus.get(0)));
+			} else {
+				result -= (Math.abs(plus.get(0)));
+			}
+		}
+		else {
+			if(minus.isEmpty())
+				result -= (Math.abs(plus.get(0)));
+			else
+				result -= (Math.abs(minus.get(0)));
+		}
+		System.out.println(result);
+
+	}
+}
