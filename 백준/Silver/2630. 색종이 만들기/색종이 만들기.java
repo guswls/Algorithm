a9459687f641e992ee6f63fb7fd9f075ffa1d72d
guswls <yodg3456@naver.com>
2022-06-28 23:09:15 +0900

    [Silver II] Title: 색종이 만들기, Time: 108 ms, Memory: 14804 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Scanner;
+
+public class Main {
+
+	static int[][] arr;
+
+	static int [] count ;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	//	Scanner sc = new Scanner(System.in);
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(bf.readLine());
+		
+		arr = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			String []str = bf.readLine().split(" ");	
+			for (int j = 0; j < N; j++) {
+				
+				arr[i][j] = Integer.parseInt(str[j]);
+			}
+		}
+
+		count = new int[2];
+		divide(0,0,N);
+		// 잘라야 되는 경우
+		System.out.println(count[0]);
+		System.out.println(count[1]);
+	}
+
+	public static void divide(int r, int c, int n) {
+		if (check(r, c, n)) {
+			count[arr[r][c]]+=1;
+		} else {
+			int newlen = n / 2;
+			for (int i = 0; i < 2; i++) {
+				for (int j = 0; j < 2; j++) {
+					divide(i * newlen + r, j * newlen + c, newlen);
+				}
+			}
+		}
+	}
+
+	public static boolean check(int r, int c, int n) {
+		int checknum = arr[r][c];
+		for (int i = r; i < r+n; i++) {
+			for (int j = c; j < c+n; j++) {
+				if (arr[i][j] != checknum) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
